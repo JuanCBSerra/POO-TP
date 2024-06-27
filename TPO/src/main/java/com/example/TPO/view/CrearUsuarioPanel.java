38555f8a002118f51e69a8553ff6191d61acbba4
Lautaro Jimenez <lauta.jimenez@gmail.com>
2024-06-27 20:02:33 -0300

    CRUD Usuarios

--- TPO/src/main/java/com/example/TPO/view/CrearUsuarioPanel.java
+++ TPO/src/main/java/com/example/TPO/view/CrearUsuarioPanel.java
@@ -1,21 +1,27 @@
 package com.example.TPO.view;
 
+import com.example.TPO.controller.UsuarioController;
+import com.example.TPO.model.Rol;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.time.LocalDate;
 
 public class CrearUsuarioPanel extends JPanel {
     /**
      *
      */
     private static final long serialVersionUID = 1L;
-    private JTextField nombreField;
-    private JTextField apellidoField;
-    private JTextField dniField;
-    private JTextField fecNacField;
     private JTextField usuarioField;
     private JTextField correoField;
+    private JTextField passwordField;
+    private JTextField nombreField;
+    private JTextField domicilioField;
+    private JTextField dniField;
+    private JTextField fecNacField;
+    private JTextField rolField;
 
     public CrearUsuarioPanel() {
         setLayout(new BorderLayout());
@@ -30,10 +36,12 @@ public class CrearUsuarioPanel extends JPanel {
 
         addFormRow(formPanel, "Usuario:", usuarioField = new JTextField(20));
         addFormRow(formPanel, "Nombre:", nombreField = new JTextField(20));
-        addFormRow(formPanel, "Apellido:", apellidoField = new JTextField(20));
         addFormRow(formPanel, "Correo:", correoField = new JTextField(20));
+        addFormRow(formPanel, "Contraseña:", passwordField = new JTextField(20));
+        addFormRow(formPanel, "Domicilio:", domicilioField = new JTextField(20));
         addFormRow(formPanel, "DNI:", dniField = new JTextField(20));
-        addFormRow(formPanel, "Fecha Nacimiento:", fecNacField = new JTextField(20));
+        addFormRow(formPanel, "Fecha Nacimiento (YYYY-MM-DD):", fecNacField = new JTextField(20));
+        addFormRow(formPanel, "Rol:", rolField = new JTextField(20));
 
         JButton btnGuardar = new JButton("Guardar");
         btnGuardar.setAlignmentX(Component.CENTER_ALIGNMENT);
@@ -46,7 +54,7 @@ public class CrearUsuarioPanel extends JPanel {
         btnGuardar.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                guardarPaciente();
+                guardarUsuario();
             }
         });
     }
@@ -55,28 +63,31 @@ public class CrearUsuarioPanel extends JPanel {
         JPanel row = new JPanel(new BorderLayout(20, 20));
         row.setMaximumSize(new Dimension(Integer.MAX_VALUE, textField.getPreferredSize().height));
         JLabel label = new JLabel(labelText);
-        label.setPreferredSize(new Dimension(120, textField.getPreferredSize().height));
+        label.setPreferredSize(new Dimension(190, textField.getPreferredSize().height));
         row.add(label, BorderLayout.WEST);
         row.add(textField, BorderLayout.CENTER);
         panel.add(row);
         panel.add(Box.createVerticalStrut(20)); // Añade un espacio vertical entre las filas
     }
 
-    private void guardarPaciente() {
+    private void guardarUsuario() {
+        String username = usuarioField.getText();
         String nombre = nombreField.getText();
-        String apellido = apellidoField.getText();
+        String correo = correoField.getText();
+        String password = passwordField.getText();
+        String domicilio = domicilioField.getText();
         String dni = dniField.getText();
+        LocalDate fecNac = LocalDate.parse(fecNacField.getText());
+        Rol rol = Rol.valueOf(rolField.getText().toLowerCase());
 
         // Validaciones básicas
-        if (nombre.isEmpty() || apellido.isEmpty() || dni.isEmpty()) {
+        if (nombre.isEmpty() || username.isEmpty() || dni.isEmpty() || password.isEmpty() || domicilio.isEmpty() || dni.isEmpty()) {
             JOptionPane.showMessageDialog(this, "Por favor, complete todos los campos.", "Error", JOptionPane.ERROR_MESSAGE);
             return;
         }
 
-        // Aquí puedes llamar a los métodos correspondientes para crear el paciente
-        // Por ejemplo: Paciente nuevoPaciente = new Paciente(nombre, apellido, dni);
-        // pacienteService.crearPaciente(nuevoPaciente);
+        UsuarioController.getInstance().agregarUsuario(username,nombre,correo,password,domicilio,dni,fecNac,rol);
 
-        JOptionPane.showMessageDialog(this, "Paciente creado con éxito.");
+        JOptionPane.showMessageDialog(this, "Usuario creado con éxito.");
     }
 }
