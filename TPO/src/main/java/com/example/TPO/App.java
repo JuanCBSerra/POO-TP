1d0f6fe0d2daef0d48755dc9690ab2efae775a18
JuanCBSerra <juaserra@uade.edu.ar>
2024-06-28 11:01:52 -0300

    fixeo de archivos innecesarios

--- TPO/src/main/java/com/example/TPO/App.java
+++ TPO/src/main/java/com/example/TPO/App.java
@@ -5,7 +5,6 @@ import com.example.TPO.view.menuLogin;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
 
         menuLogin frame = new menuLogin("Sistema de Gestión de Laboratorio");
         frame.setVisible(true);
